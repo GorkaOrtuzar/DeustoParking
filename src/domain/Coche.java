f04c12c5db8b5657e3df2e5ca1549cc9eeb94cc6
ainaramaroto <ainara.maroto@opendeusto.es>
2024-10-15 23:02:11 +0200

    Clase Usuario y Clase Coche

--- src/domain/Coche.java
+++ src/domain/Coche.java
@@ -1,5 +1,44 @@
 package domain;
 
-public class Coche {
+import java.util.Objects;
 
+public class Coche {
+	private String marca;
+	private String modelo;
+	private String matricula;
+	
+	public Coche() {
+		super();
+	}
+
+	public Coche(String marca, String modelo, String matricula) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+		this.matricula = matricula;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(marca, matricula, modelo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Coche other = (Coche) obj;
+		return Objects.equals(marca, other.marca) && Objects.equals(matricula, other.matricula)
+				&& Objects.equals(modelo, other.modelo);
+	}
+
+	@Override
+	public String toString() {
+		return "Coche [marca=" + marca + ", modelo=" + modelo + ", matricula=" + matricula + "]";
+	}
+	
 }
--- src/domain/Usuario.java
+++ src/domain/Usuario.java
@@ -1,5 +1,91 @@
 package domain;
 
-public class Usuario {
+import java.util.Objects;
 
+public class Usuario {
+	private String nombre;
+	private String apellido;
+	private String tlf;
+	private String dni;
+	private String contrasenia;
+	
+	public Usuario() {
+		super();
+	}
+
+	public Usuario(String nombre, String apellido, String tlf, String dni, String contrasenia) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.tlf = tlf;
+		this.dni = dni;
+		this.contrasenia = contrasenia;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getTlf() {
+		return tlf;
+	}
+
+	public void setTlf(String tlf) {
+		this.tlf = tlf;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(apellido, contrasenia, dni, nombre, tlf);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(apellido, other.apellido) && Objects.equals(contrasenia, other.contrasenia)
+				&& Objects.equals(dni, other.dni) && Objects.equals(nombre, other.nombre)
+				&& Objects.equals(tlf, other.tlf);
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [nombre=" + nombre + ", apellido=" + apellido + ", tlf=" + tlf + ", dni=" + dni
+				+ ", contrasenia=" + contrasenia + "]";
+	}
+	
+	
 }
