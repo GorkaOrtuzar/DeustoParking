a98700515bdaa44c921cc7b1fdee45828cce649a
GorkaOrtuzar <ortuzar.gorka@opendeusto.es>
2024-10-27 21:26:33 +0100

    Jtable creada de la ventnana mis reservas

--- src/gui/VentanaMisReservas.java
+++ src/gui/VentanaMisReservas.java
@@ -1,31 +1,63 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.HeadlessException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
 public class VentanaMisReservas extends JFrame {
 
-	private JPanel pCentro, pCentroN, pCentroS, pSur, pNorte;
+	private JPanel pCentro, pSur, pNorte;
 	private JButton btnCerrar;
-	private JTextArea txtReserva;
+	private JLabel lblReserva;
 	
-	private ModeloTablaReserva modeloTabla; 
+	private DefaultTableModel modeloTabla; 
 	private JTable tabla; 
 	private JScrollPane scrollTabla;
 	
 	private JFrame vActual;
 	public VentanaMisReservas(JFrame va) {
 		
+		pNorte = new JPanel();
 		pSur = new JPanel();
+		pCentro = new JPanel();
+		pCentro.setBorder(new EmptyBorder(100, 100, 100, 100 ));
+
+		getContentPane().add(pNorte, BorderLayout.NORTH);
 		getContentPane().add(pSur, BorderLayout.SOUTH);
+		getContentPane().add(pCentro, BorderLayout.CENTER);
+		
+
+		//Titulo
+		lblReserva = new JLabel("MIS RESERVAS");
+		lblReserva.setFont(new Font(Font.DIALOG, Font.ITALIC, 18));
+		pNorte.add(lblReserva);
+		
+		//Tabla
+		modeloTabla = new DefaultTableModel();
+		tabla = new JTable(modeloTabla);
+		scrollTabla = new JScrollPane(tabla);
+		scrollTabla.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollTabla.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		String [] titulos = {"FECHA DE ENTRADA","FECHA DE SALIDA","NOMBRE DEL PARKING", "PRECIO"};
+		modeloTabla.setColumnIdentifiers(titulos);
+		tabla.getTableHeader().setReorderingAllowed(false);
+		
+		
+		pCentro.add(scrollTabla);
+		
+		
+		
 		
 		//Boton cerrar
 		btnCerrar = new JButton("Cerrar");
