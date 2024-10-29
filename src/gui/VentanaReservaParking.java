a6f9b6e94b1e2d03aa7984523f5bb167c9e0b3ef
ainaramaroto <ainara.maroto@opendeusto.es>
2024-10-29 18:41:12 +0100

    Dar acciones a los botones

--- src/gui/VentanaReservaParking.java
+++ src/gui/VentanaReservaParking.java
@@ -1,7 +1,15 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.GraphicsEnvironment;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,6 +18,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
+
+import domain.Reserva;
 
 public class VentanaReservaParking extends JFrame{
 	
@@ -23,10 +33,16 @@ public class VentanaReservaParking extends JFrame{
 	private JTable tabla; 
 	private JScrollPane scrollTabla;
 	
+	private JFrame vActual;
+	private JFrame vAnterior;
 	
 	
-	public VentanaReservaParking() {
+	
+	public VentanaReservaParking(JFrame va, Reserva r) {
 		super();
+		vActual = this;
+		vAnterior = va;
+		
 		
 		//Tamaño Ventana
 		setBounds(300, 200, 600, 400);
@@ -45,10 +61,14 @@ public class VentanaReservaParking extends JFrame{
 		txtReserva = new JTextArea();
 		
 		btnAtras = new JButton("Atrás");
+		
+		btnAtras.addActionListener((e)->{
+			vActual.dispose();
+			vAnterior.setVisible(true);
+			
+		});
+		
 		btnReservar = new JButton("Reservar");
-		btnSiguiente = new JButton("Siguiente");
-		
-		
 		
 		
 		//Añadimos paneles al panel central
@@ -69,19 +89,94 @@ public class VentanaReservaParking extends JFrame{
 		pCentroS.add(txtReserva);
 		pCentroS.add(btnReservar);
 		pSur.add(btnAtras);
-		pSur.add(btnSiguiente);
 		
 		getContentPane().add(pNorte, BorderLayout.NORTH);
 		getContentPane().add(pCentro, BorderLayout.CENTER);
 		getContentPane().add(pSur, BorderLayout.SOUTH);
 		
-	setVisible(true);
+		tabla.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int fila = tabla.getSelectedRow();
+				if(fila!=-1) {
+					String nomParking = (String) tabla.getValueAt(fila, 0);
+					int numPlazasLibres = (int) tabla.getValueAt(fila, 1);
+					float precio = (float) tabla.getValueAt(fila, 2);
+				}
+				
+			}
+		});
 		
+		//btnRservar guarda los datos seleccionados de la tabla, y los guarda en r (Reserva)  -- TERMINAR --
+		btnReservar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				vActual.dispose(); 
+				new VentanaIngresarDatos(vActual, r);
+				
+			}
+		});
+		
+
+		
+		//Tamaño de la ventana
+		int anchoP = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getWidth();
+        int altoP = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getHeight();
+        setSize(anchoP, altoP);
+        setExtendedState(MAXIMIZED_BOTH);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(50, 50, 1600, 800);
+		setTitle("Reservar plaza de parking");
+		setVisible(true);
 	}
 	
-	
-	public static void main(String[] args) {
-		VentanaReservaParking v = new VentanaReservaParking();
+
+	private void cargarTablaDatos() {
+		File f = new File("ficheros/datosParking.txt");
+		
+		if(f.exists()) {
+			try {
+				Scanner sc = new Scanner(f);
+				while(sc.hasNextLine()) { 
+					String linea = sc.nextLine(); 
+					String [] datos = linea.split(";");
+					Object [] fila = {datos[0], datos[1], datos[2]};
+					modeloTabla.addRow(fila);
+				}
+				sc.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
 	}
 
 }
