19766ff1028fdc98a998c4fb309f6ffe415e54d2
ainaramaroto <ainara.maroto@opendeusto.es>
2025-01-07 10:42:21 +0100

    cambios bd

--- src/domain/BD.java
+++ src/domain/BD.java
@@ -206,8 +206,8 @@ public class BD {
 	
 	//Insertar Reserva
 	public static void insertarReserva(Connection con, Reserva reserva){
-			String sql = String.format("INSERT INTO Reserva VALUES('%s','%s','%s','%s','%s','%s','%s','%s')", 
-					reserva.getCiudad(), reserva.getDni(),reserva.getMatricula(), reserva.getNomParking(), reserva.gethLlegada(),reserva.gethSalida(), reserva.getNumPlaza(),reserva.getPrecioTotal());
+			String sql = String.format("INSERT INTO Reserva VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s')", 
+					reserva.getCiudad(), reserva.getDni(),reserva.getMatricula(), reserva.getNomParking(), reserva.gethLlegada(),reserva.gethSalida(), reserva.getNumPlaza(),reserva.getSeccion(), reserva.getPrecioTotal());
 			try {
 				Statement st = con.createStatement();
 				st.executeUpdate(sql);
